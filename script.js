dd8edefb47d45a914f1d3e438a1c83971870d623
Nikolas Oiko <nikosoikonomou88@gmail.com>
2025-07-28 00:28:30 +0300

    calculate score. Track highest score

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 'use strict';
-const againButton = document.querySelector('.again');
-const guessInput  = document.querySelector('.guess');
-const inputField = document.querySelector('.check');
 const randomNumberPlaceHolder = document.querySelector('.number');
+const againButton = document.querySelector('.again');
+const guessInput = document.querySelector('.guess');
+const inputField = document.querySelector('.check');
 const message = document.querySelector('.message');
+const score = document.querySelector('.score');
+const highScore = document.querySelector('.highscore');
 
 let winningNumber = getRandomNumber();
+let scoreValue = Number(document.querySelector('.score').textContent);
+let highScoreValue = Number(document.querySelector('.highscore').textContent);
 // Handlers
 const guessedNumberHandler = () => {
   const guessedNumber = Number(document.querySelector('.guess').value);
   console.log('winningnumber', winningNumber);
-  console.log('quessed', guessedNumber);
-  if (!guessedNumber) message.textContent = 'Select a number!';
+  //   console.log('quessed', guessedNumber);
+  if (!guessedNumber) message.textContent = '⚠️ Select a number!';
   else if (winningNumber === guessedNumber) {
     console.log(true);
     message.textContent = '🎉 Correct Number!';
     randomNumberPlaceHolder.textContent = winningNumber;
+    highScore.textContent = score.textContent;
   } else {
-    console.log(false);
-    if ( guessedNumber > winningNumber ) message.textContent = 'Too Big!';
-    if ( guessedNumber < winningNumber ) message.textContent = 'Too Small!';
-    
+    if (guessedNumber > winningNumber) message.textContent = '📈 Too Big!';
+    if (guessedNumber < winningNumber) message.textContent = '📉 Too Small!';
+    scoreValue -= 1;
+    score.textContent = scoreValue;
+    console.log('scoreValue', scoreValue);
+    console.log('highScoreValue', highScoreValue);
   }
 };
 
@@ -29,6 +36,8 @@ const resetGameHandler = () => {
   message.textContent = 'Start guessing again...';
   randomNumberPlaceHolder.textContent = '?';
   guessInput.value = '';
+  score.textContent = 20;
+  scoreValue = 20;
 };
 
 inputField.addEventListener('click', guessedNumberHandler);
