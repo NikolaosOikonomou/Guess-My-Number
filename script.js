67a2127b564ff462cd0ac000dba6b43a40571721
Nikolas Oiko <nikosoikonomou88@gmail.com>
2025-07-27 23:35:46 +0300

    reset game

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 'use strict';
-const inputField = document.querySelector('.check');
 const againButton = document.querySelector('.again');
+const guessInput  = document.querySelector('.guess');
+const inputField = document.querySelector('.check');
+const randomNumberPlaceHolder = document.querySelector('.number');
+const message = document.querySelector('.message');
 
 let winningNumber = getRandomNumber();
-let randomNumberPlaceHolder = document.querySelector('.number');
-let message = document.querySelector('.message');
-
 // Handlers
 const guessedNumberHandler = () => {
-  let quessedNumber = Number(document.querySelector('.guess').value);
+  const guessedNumber = Number(document.querySelector('.guess').value);
   console.log('winningnumber', winningNumber);
-  console.log('quessed', quessedNumber);
-  if (winningNumber === quessedNumber) {
+  console.log('quessed', guessedNumber);
+  if (!guessedNumber) message.textContent = 'Select a number!';
+  else if (winningNumber === guessedNumber) {
     console.log(true);
     message.textContent = '🎉 Correct Number!';
     randomNumberPlaceHolder.textContent = winningNumber;
   } else {
     console.log(false);
-    if ( quessedNumber > winningNumber ) message.textContent = 'Too Big!';
-    if ( quessedNumber < winningNumber ) message.textContent = 'Too Small!';
+    if ( guessedNumber > winningNumber ) message.textContent = 'Too Big!';
+    if ( guessedNumber < winningNumber ) message.textContent = 'Too Small!';
     
   }
 };
@@ -27,6 +28,7 @@ const resetGameHandler = () => {
   winningNumber = getRandomNumber();
   message.textContent = 'Start guessing again...';
   randomNumberPlaceHolder.textContent = '?';
+  guessInput.value = '';
 };
 
 inputField.addEventListener('click', guessedNumberHandler);
